64e7dc482259d1fea0a6beff5a7d0f865a7992b1
KaidenTheCopyCat <helenagarciaviles@gmail.com>
2023-03-07 13:45:05 +0100

    First commit. Bases dels exercicis i nivells

---
+++ TascaS101/src/n1exercici3/Main.java
@@ -0,0 +1,27 @@
+package n1exercici3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Explica’ns com ho has fet!
+
+A més del codi que entregaràs per a resoldre aquest nivell, volem que ens l'expliquis i demostris com s'executa.
+
+L'objectiu és que practiquis el que serà el teu dia a dia treballant integrat en un equip de desenvolupament on, periòdicament, hauràs de detallar les teves propostes o justificar les decisions tècniques que hagis pres.
+
+Aquest exercici et permetrà treballar a ordenar idees i ser capaç de resumir i comunicar allò que consideris més rellevant per a la teva audiència. A la IT Academy és la persona encarregada de la mentoria, però en el futur serà l’equip de desenvolupament en el que t’integris.
+
+Per això, et demanem que gravis un vídeo, que pots incloure en el mateix Repositori de Git on has d'entregar el codi, o en un altre diferent (en aquest cas, no t’oblidis d’incloure l’enllaç).
+
+El vídeo ha de complir els següents requisits:
+
+Ha d'incloure la gravació de la pantalla del teu equip on puguis anar mostrant i detallant el que et sembli més rellevant del codi que has escrit, i demostrar el seu funcionament executant-lo.
+L'àudio ha de tenir un mínim de qualitat perquè s'entengui.
+No hi ha establerta una durada mínima, però no hauria de ser superior als cinc minuts, tret d’exercicis molt llargs.
+No cal que sigui un vídeo “professional”, ni que l'editis per afegir portades, ni epílegs, i tampoc cal que hi surtis tu.
+		 */
+
+	}
+
+}
